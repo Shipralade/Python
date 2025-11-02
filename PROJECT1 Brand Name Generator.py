6040661c22b6c7d7369c0e3670bc446bb2f400e4
Shipra Lade <143022072+Shipralade@users.noreply.github.com>
2025-11-02 13:32:14 +0530

    Add files via upload

---
+++ PROJECT1 Brand Name Generator.py
@@ -0,0 +1,6 @@
+# A Brand Name Generator
+print("Welcome to the Brand Name Generator!")
+print("Let's create a Brand Name For You")
+Nickname=input("What is your Nickname?\n")
+Color=input("What is your favorite color?\n")
+print("Your Brand Name could be " + Nickname + " " + Color + "!")
